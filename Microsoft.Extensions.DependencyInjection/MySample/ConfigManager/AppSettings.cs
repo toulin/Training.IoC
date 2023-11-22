7dcb24fe59e154d82d4f6fb8ff5ea8e66441c6b6
toulin <toulin@cmoney.com.tw>
2023-11-22 10:39:39 +0800

    add 加入版控

---
+++ Microsoft.Extensions.DependencyInjection/MySample/ConfigManager/AppSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//需額外參考 System.Configuration.dll
+namespace MySample.ConfigManager
+{
+    public class AppSettings
+    {
+        public string Setting1 { get; set; }
+        public int Setting2 { get; set; }
+
+        public static AppSettings LoadFromConfig()
+        {
+            return new AppSettings
+            {
+                Setting1 = ConfigurationManager.AppSettings["Setting1"],
+                Setting2 = int.Parse(ConfigurationManager.AppSettings["Setting2"])
+            };
+        }
+    }
+}
---
+++ Microsoft.Extensions.DependencyInjection/MySample/MyDataProviderA.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySample
+{
+    internal class MyDataProviderA:IDataProvider
+    {
+        private double CreateSeconds = 0;
+
+        public MyDataProviderA() 
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            CreateSeconds = Program.Watch.Elapsed.TotalSeconds;
+        }
+
+        public string GetData(string name)
+        {
+            return $"Hello {name},   MyDataProviderA's HashCode={this.GetHashCode()}, 程式啟動後過幾秒實例化{CreateSeconds}";
+        }
+    }
+}
